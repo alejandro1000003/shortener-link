d23f3a38b60d2dcdf5290e8adcdf12973e893c01
Alejandro <alejandro1000003@gmail.com>
2025-05-29 13:41:37 +0200

    cambio de ruta

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.get('/:code', (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
