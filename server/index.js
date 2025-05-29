dd2545cc41b3ad23b7027b0d360a3b511dda9b6a
Alejandro <alejandro1000003@gmail.com>
2025-05-29 13:47:41 +0200

    cambio cambio

--- server/index.js
+++ server/index.js
@@ -72,9 +72,12 @@ app.get('/:code', (req, res) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, '../dist')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+const distPath = path.join(__dirname, '../dist');
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(port, () => {
